2ef3954e4fd0c43361b69f3d77fa2c462f4b6125
luuk600 <119942867+luuk600@users.noreply.github.com>
2024-01-20 11:58:58 +0100

    cat cannon
    dit is 1 van de laatste opdrachten van scripting dus leek me handig om deze te maken

---
+++ amitieproject/dice game/cat cannon.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int ammo = 6;
+    int clear = 0;
+    int mag = 30;
+    char shoot;
+    bool magfull = true;
+    bool ammofilll = true;
+
+
+    cout << "type r to reload and s to shoot." << endl;
+    cin >> shoot;
+    while (magfull) 
+    {
+        if (mag < 1) 
+        {
+            cout << "you have shot all your cats good job" << endl;
+            magfull = false;
+        }
+        if (shoot != 'r' && shoot != 's')
+        {
+            cin >> shoot;
+        }
+        if (shoot == 's' && magfull && ammofilll)
+        {
+            shoot = 'n';
+            ammo--;
+            clear++;
+            cout << "ammo = " << ammo << endl;
+            cin >> shoot;
+        }
+        if (shoot == 'r' && mag > 5)
+        {
+            shoot = 'n';
+            ammo += clear;
+            mag -= clear;
+            cout << ammo << endl;
+            cout << mag << endl;
+            clear = 0;
+            cin >> shoot;
+            ammofilll = true;
+            if (ammo == 0 && mag != 0)
+            {
+                cout << "ammo = 0" << endl;
+                cout << ammo << endl;
+                cout << mag << endl;
+            }
+        }
+        if (shoot == 'r' && mag < 6)
+        {
+            shoot = 'n';
+            ammo = mag;
+            cout << ammo << endl;
+            cout << mag << endl;
+            mag = 0;
+        }
+        if (ammo == 0 && magfull)
+        {
+            ammofilll = false;
+            cout << "you need to press r" << endl;
+            cin >> shoot;
+        }
+    }
+}
+// Debug program: F5 or Debug > Start Debugging menu
